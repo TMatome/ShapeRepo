8972110f107ecac5473745c5d330b6c14b807d3f
TMatome <46716771+TMatome@users.noreply.github.com>
2019-01-18 13:10:04 +0200

    This is circle features

---
+++ Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleFeature
+{
+    class Circle
+    {    
+        public void calculateArea()
+        {
+            Console.WriteLine("Please enter the radius: ");
+            float radius = float.Parse(Console.ReadLine());
+            Console.WriteLine("The Area of a Circle with radius {0} is {1}", radius, (radius * radius) * (float)Math.PI);
+        }
+    }
+}
